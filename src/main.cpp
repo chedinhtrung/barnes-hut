50e276c67e795b179f6810edec62ec91031a8632
steve <stevenlarry1710@gmail.com>
2025-11-29 16:38:47 +0100

    added csv writing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 #include "simulation.hpp"
 #include "initial_conditions.hpp"
@@ -7,46 +8,51 @@
 
 int main() {
     // 1. Simulation settings
-    const std::size_t N = 100; // Number of bodies
+    const int N = 100; // Number of bodies
     const double G = 1.0; // Gravitational constant, scaled units, don't use the real G here because it might lead to underflow
-    const double dt = 0.01; // Timestep size
-    const int steps = 1000; // Number of timesteps to simulate
+    const double dt = 0.002; // Timestep size
+    const int steps = 2000; // Number of timesteps to simulate
     const double theta = 0.5; // Threshold for Barnes-Hut
 
     // 2. Generate initial bodies
-    std::vector<Body> bodies = randomInitialization(
+    std::vector<Body> bodies_bh = randomInitialization(
         N,
         1.0, 10.0, // Mass range: [1, 10]
         10.0, // Position range: [-10, 10]
         0.1, // Velocity range: [-0.1, 0.1]
         1234 // Random seed
     );
-
     // 3. Create two Simulation objects with the same initial state
-    Simulation simNaive(bodies, G);
-    Simulation simBH(bodies, G);
+    Simulation simBH(bodies_bh, G);
+
+    // Open csv file
+    FILE* barneshut_csv = fopen("../results/barnes-hut.csv", "w");
+
+    const auto& bodiesBH = simBH.getBodies();
+    // Write the head of the dataframe
+    for (const Body& body : bodiesBH){
+        fprintf(barneshut_csv, "step, time, body, m, x, y, z, vx, vy, vz");
+    }
 
     // 4. Run both simulations side-by-side
     for (int step = 0; step < steps; ++step) {
         // Advance one step with each method
-        simNaive.step(dt); // Brute-force O(N^2)
         simBH.stepBarnesHut(dt, theta); // Barnes-Hut
 
+        for (int j=0; j<bodies_bh.size(); j++){
+                const Body b = bodiesBH[j];
+                                    //"step, time, body, m, x, y, z, vx, vy, vz"
+                fprintf(barneshut_csv, "%i, %.4f, %i, %.2f, %.4f, %4f, %4f, %4f, %4f, %4f \n",
+                                        step, step*dt, j, b.mass, b.position.x, b.position.y, b.position.z, b.velocity.x, b.velocity.y, b.velocity.z);
+        }
         // Dummy logging every 100 steps
         if (step % 100 == 0) {
-            const auto& bodiesNaive = simNaive.getBodies();
             const auto& bodiesBH = simBH.getBodies();
 
-            if (!bodiesNaive.empty() && !bodiesBH.empty()) {
-                const Body& bNaive = bodiesNaive[0];
+            if (!bodiesBH.empty()) {
                 const Body& bBH = bodiesBH[0];
 
                 std::cout << "Step " << step << "\n";
-
-                std::cout << "  Naive first body position = ("
-                          << bNaive.position.x << ", "
-                          << bNaive.position.y << ", "
-                          << bNaive.position.z << ")\n";
 
                 std::cout << "  Barnes-Hut first body position = ("
                           << bBH.position.x << ", "
